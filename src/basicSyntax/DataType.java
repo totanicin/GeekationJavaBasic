68ce0c28f270b17f3bfc1b884bbff43b2ffcda3c
totanicin <t.otani.cin2012@gmail.com>
2024-04-26 11:43:34 +0900

    データ型の実装

---
+++ src/basicSyntax/DataType.java
@@ -0,0 +1,29 @@
+package basicSyntax;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String hello = "こんにちは" ;
+		String iam = "私は" ;
+		String geek = "Geek君です。" ;
+		String text = hello + iam + geek ;
+		System.out.println(text) ;
+		
+		int ten = 10 ;
+		System.out.println(ten) ;
+		
+		String twenty = "20" ;
+		String twenty_four = "24" ;
+		String two_thousand_twenty_four = twenty + twenty_four ;
+		System.out.println(two_thousand_twenty_four) ;
+		
+		double pi = 3.14 ;
+		System.out.println(pi) ;
+		
+		boolean truth = true ;
+		System.out.println(truth) ;
+		
+	}
+
+}
---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,8 @@
+package basicSyntax;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(true);
+    System.out.println(false);
+  }
+}
