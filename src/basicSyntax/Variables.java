8afb07f86f32afddc833ff51c2406c7b94608d0e
totanicin <t.otani.cin2012@gmail.com>
2024-05-07 10:09:33 +0900

    要件の実装

---
+++ src/basicSyntax/Variables.java
@@ -0,0 +1,21 @@
+package basicSyntax;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int number1 = 10;
+		double number2 = 0.5;
+		
+		String favoriteInt = "私の好きな整数は";
+		String favoriteDouble = "私の好きな少数は";
+		
+		System.out.println(number1);
+		System.out.println(number2);
+		
+		System.out.println(favoriteInt + number1 + "です。");
+		System.out.println(favoriteDouble + number2 + "です。");
+
+	}
+
+}
